04f06a7a9521dc1829153f7c030bcf1896192d02
unknown <abdalaagustineze@gmail.com>
2026-05-07 16:33:28 -0300

    pantalla de carga!!

---
+++ loader/loader.js
@@ -0,0 +1,16 @@
+// ======================================================
+// ===================== LOADER =========================
+// ======================================================
+
+window.addEventListener('load', () => {
+
+    const loader =
+        document.getElementById('loader');
+
+    setTimeout(() => {
+
+        loader.classList.add('hidden');
+
+    }, 1800);
+});
+
